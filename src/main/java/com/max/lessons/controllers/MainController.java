252ab42880c27c22ce404b505f12ae1be27c4e1f
Max <hacker_@list.ru>
2018-05-25 00:28:54 +0300

    add html

--- src/main/java/com/max/lessons/controllers/MainController.java
+++ src/main/java/com/max/lessons/controllers/MainController.java
@@ -1,12 +1,18 @@
 package com.max.lessons.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class MainController {
     @GetMapping("/helloWorld")
+    @ResponseBody
     public String helloWorld() {
         return "hello world";
+    }
+    @GetMapping("/index")
+    public String index() {
+        return "index";
     }
 }
